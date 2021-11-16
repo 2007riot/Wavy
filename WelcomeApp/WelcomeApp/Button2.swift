b497d8d82907d0eab45b5f6b7b6ddd04709bb472
2007riot <73304608+2007riot@users.noreply.github.com>
2021-11-16 18:20:29 +0100

    Uploaded files
    first upload by galina

---
+++ WelcomeApp/WelcomeApp/Button2.swift
@@ -0,0 +1,37 @@
+//
+//  Button2.swift
+//  WelcomeApp
+//
+//  Created by Galina Aleksandrova on 16/11/21.
+//
+
+import SwiftUI
+
+struct Button2: View {
+    var body: some View {
+        Text("Should be a button")
+//        Button
+//        .contentShape(Rectangle())
+//        Button(action: {}) {
+//            /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+//        }
+//        Button {
+//            print("Button pressed")
+//        } label: {
+//            Text("Press Me")
+//                .padding(20)
+//        }
+//        .contentShape(Rectangle())
+//
+//
+//        Button("Back") {
+//            print("back button tapped")
+//        }
+    }
+}
+
+struct Button2_Previews: PreviewProvider {
+    static var previews: some View {
+        Button2()
+    }
+}
---
+++ WelcomeApp/WelcomeApp/popoverOnboardingQuestions.swift
@@ -0,0 +1,20 @@
+//
+//  popoverOnboardingQuestions.swift
+//  WelcomeApp
+//
+//  Created by Galina Aleksandrova on 16/11/21.
+//
+
+import SwiftUI
+
+struct popoverOnboardingQuestions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct popoverOnboardingQuestions_Previews: PreviewProvider {
+    static var previews: some View {
+        popoverOnboardingQuestions()
+    }
+}
