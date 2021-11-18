37765305c717af8a0bec545606e2b9604824c8b7
Welcome <2007riot@gmail.com>
2021-11-18 17:12:12 +0100

    added TabbedView

--- WelcomeApp/WelcomeApp/Memories/Views/TabbedView.swift
+++ WelcomeApp/WelcomeApp/Memories/Views/TabbedView.swift
@@ -10,8 +10,20 @@ import SwiftUI
 struct TabbedView: View {
     var body: some View {
         
-        
-        Text("name")
+        TabView {
+            FindScreen()
+                .tabItem {
+                    Text("Find")
+                    Image(systemName: "person.3.fill")
+                }
+            memoryScreen()
+                .tabItem {
+                    Text("Memories")
+                    Image(systemName: "photo.on.rectangle.angled")
+                }
+            
+           
+        }
     }
 }
 
